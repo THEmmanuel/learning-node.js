8fe8edcc608e75a7e090d38ac01334bc7adae2d1
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-12-10 07:38:51 +0100

    os module

--- first_node_app/app.js
+++ first_node_app/app.js
@@ -1,1 +1,11 @@
-const path = require('path');
+const os = require('os');
+
+let freeMem = os.freemem();
+let uptime = os.uptime();
+let userinfo = os.userInfo();
+let totalMem = os.totalmem();
+
+console.log(`Total Memory: ${totalMem}`)
+console.log(`Free Memory: ${freeMem}`)
+console.log(`user info: ${userinfo}`)
+console.log(`uptime: ${uptime}`)
